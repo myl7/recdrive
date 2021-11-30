b53cb12c0356621e9ceb8e9bcbef42ba8906cd11
myl7 <myl@myl.moe>
2021-11-30 22:31:42 +0800

    feat: Add http client into options

--- options.go
+++ options.go
@@ -1,14 +1,21 @@
 package recdrive
 
+import "net/http"
+
 type Options struct {
 	// `x-auth-token` field in headers, used to auth by recdrive
 	AuthToken   string
 	ApiEndpoint string
+	HttpClient  *http.Client
 }
 
 func (opt Options) Build() Options {
 	if opt.ApiEndpoint == "" {
 		opt.ApiEndpoint = apiEndpointDefault
+	}
+
+	if opt.HttpClient == nil {
+		opt.HttpClient = http.DefaultClient
 	}
 
 	return opt
